459641d074db5ec670b168421fba051e29d8581f
Ascalonic <ascalonic@gmail.com>
2017-07-02 01:12:16 +0530

    Check for human input making sense

--- genisys_chat.py
+++ genisys_chat.py
@@ -1,5 +1,6 @@
 import nltk
 import json
+import re
 
 class BotIO(object):
 
@@ -41,6 +42,15 @@ class DictMgr(object):
 
 		return data
 
+################# Initialise the dictionaries ##################
+
+dict_mgr = DictMgr()
+knowledge_db = dict_mgr.read_dict('knowledge')
+words_db = dict_mgr.read_dict('words')
+context_db = {}
+
+###############################################################
+
 
 class NLChunker(object):
 
@@ -53,30 +63,41 @@ class NLChunker(object):
 
 class BotAI(object):
 
+	#check whether test_string contains only nouns (NN.*)
+	def not_only_nouns(self, test_string):
+		matchObj = False
+		for tagged_unit in test_string:
+			if "NN" not in tagged_unit[1]:
+				return True
+		return False
+
 	#process the input
 	def bot_process (self, response):
 		nl_processor = NLProcessor()
 		resp_tok = nl_processor.nl_tokenize(response) #tokenize
 		resp_tagged = nl_processor.nl_tag(resp_tok)   #POS tag
+
+		if self.not_only_nouns(resp_tagged):
+		#Not only nouns
+			return "No only nouns"
+		else:
+		#Only Nouns
+			if len(context_db)==0:
+				#things doesn't make any sense
+				return "What are you talking about?!"
+
 		nl_chunker = NLChunker()
 		print(nl_chunker.nl_chunkparse(resp_tagged, "NP: {<DT>?<JJ>*<NN>}"))
 
-
-
 bot_io = BotIO()
 bot_ai = BotAI()
-
-dict_mgr = DictMgr()
-knowledge_db = dict_mgr.read_dict('knowledge')
-words_db = dict_mgr.read_dict('words')
-context_db = {}
 
 bot_io.bot_say("Hi, this is Lois. Say \'#quit\' when you want to quit")
 
 said = ""
 while(said!="#quit"):
 	said = bot_io.bot_listen()
-	bot_ai.bot_process(said)
+	bot_io.bot_say(bot_ai.bot_process(said))
 
 
 bot_io.bot_say("Goodbye, It was fun talking to you!")
